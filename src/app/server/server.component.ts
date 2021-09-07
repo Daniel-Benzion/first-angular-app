716f804c02628c2e91adced6d3b5a55aaa50c2b3
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-09-07 16:26:33 -0500

    ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,20 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .online {
+      color: white;
+    }
+  `]
 })
 export class ServerComponent {
-  private serverId: number = 10;
-  private serverStatus: string = "offline";
+  serverId: number = 10;
+  serverStatus: string = "offline";
 
   constructor() {
     this.serverStatus = Math.random() > 0.5 ? "online" : "offline";
   }
 
-  public getServerId() {
+  getServerId() {
     return this.serverId;
   }
-  public getServerStatus() {
+  getServerStatus() {
     return this.serverStatus;
   }
 
