cda03035cf7060f9ed09c50e80e1b18676456bef
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-09-07 16:06:04 -0500

    ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,12 +8,19 @@ export class ServerComponent {
   private serverId: number = 10;
   private serverStatus: string = "offline";
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? "online" : "offline";
+  }
 
   public getServerId() {
     return this.serverId;
   }
   public getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.getServerStatus() === "online" ? "green" : "red";
   }
 }
 
